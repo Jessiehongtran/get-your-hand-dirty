126179b5720fda5ddafe46bea1f7fe7a553cfe8c
Your Name <you@example.com>
2020-03-23 15:42:22 -0700

    arranged content for a project

--- app/src/components/project.jsx
+++ app/src/components/project.jsx
@@ -21,13 +21,68 @@ class Project extends React.Component {
             }
         
         }
-
+        console.log('clickedProject', clickedProject)
 
         return (
             <div>
-                <img src={clickedProject.image} alt="project-img"/>
-                <p>Project: {clickedProject.name}</p>
-                <p>Tool: {clickedProject.tool}</p>
+                <div className="project-intro">
+                    <p>Project: {clickedProject.name}</p>
+                    <p>{clickedProject.description}</p>
+                    <img src={clickedProject.image} alt="project-img"/>
+                </div>
+                <div className="demographic">
+                    <p>Tool: {clickedProject.tool}</p>
+                    <p>AGE: {clickedProject.demographic.age}</p>
+                    <p>DIFFICULTY: {clickedProject.demographic.difficulty} </p>
+                    <p>DURATION: {clickedProject.demographic.duration}</p>
+                </div>
+                <div className="learning">
+                    What students will take away:
+                    {clickedProject.details.learning.map(point => (
+                        <ul>
+                           
+                            <li>{point}</li>
+                        </ul>
+                    )
+                    )}
+                </div>
+                <div className="approach">
+                    Thought process approach
+                    {clickedProject.details.approach.map(point => (
+                        <ul>
+                            <li>{point}</li>
+                        </ul>
+                    )
+                    )}
+                </div>
+                <div className="triggering-questions">
+                    Triggering questions to ask
+                    {clickedProject.details.triggering_questions.map(point => (
+                        <ul>
+                            <li>{point}</li>
+                        </ul>
+                    )
+                    )}
+                </div>
+                <div className="key_robotics_components">
+                    Key robotics components
+                    {clickedProject.details.key_robotics_components.map(point => (
+                        <ul>
+                            <li>{point}</li>
+                        </ul>
+                    )
+                    )}
+                </div>
+                <div className="key_programming_concepts">
+                    Key programming concepts
+                    {clickedProject.details.key_programming_concepts.map(point => (
+                        <ul>
+                            <li>{point}</li>
+                        </ul>
+                    )
+                    )}
+                </div>
+
             </div>
         )
 
